b3652ecb7cbce355ab71a1b845458ba17391ca09
yUan44 <yyx_curryss@163.com>
2019-03-18 09:46:06 +0800

    canvas粒子运动--vue

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <button class="small-chiken-root" @click="parentssss()">小鸡跟<i>say</i></button>
+    <HelloWorld ref="childddd" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    parentssss () {
+      this.$refs.childddd.isHhhh()
+    }
+  }
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  .small-chiken-root {
+    border: 0;
+    padding: 4px 10px;
+    background: #26f1e7;
+    outline: none;
+    color: #fff;
+    border-radius: 7px;
+    cursor: pointer;
+    z-index: 999;
+    position: absolute;
+    top: 20px;
+    left: 50%;
+    margin-left: -15px;
+  }
+  .small-chiken-root i {
+    font-style: normal;
+    font-weight: bold;
+    display: inline-block;
+    margin-left: 4px;
+    color: #2c3e50;
+    vertical-align: middle;
+    font-size: 16px
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <section id="ci-particles">
+        <canvas id="canvas"></canvas>
+        <h1 id="headline">kimetiiiiiii</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+import { hhhh } from "../assets/demo.js";
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+      isHhhh (){
+        hhhh()
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello {
+      width: 100vw;
+      height: 100vh;
+      background-color: #212121;
+  }
+
+  hello section canvas {
+      width: 100vw;
+      height: 100vh;
+  }
+</style>
